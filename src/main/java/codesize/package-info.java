00d7564378ce618777c6266578536e881eaf66fc
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-10-25 16:54:00 +0200

    Updated package-info

--- src/main/java/codesize/package-info.java
+++ src/main/java/codesize/package-info.java
@@ -1,25 +1,12 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*******************************************************************************
- * Copyright (c) 2002 - 2018 Christian D. Schnell, Flemming N. Larsen
+ * Copyright (c) 2002 Christian D. Schnell, 2008 Flemming N. Larsen
  * All rights reserved.
  *
  * @since 1.0
- * @author Christian D. Schnell and Flemming N. Larsen
  * @version 1.2
- *******************************************************************************/
+ * @author Christian D. Schnell and Flemming N. Larsen
+ */
+/**
+ * Contains the Codesize tool.
+ */
 package codesize;
